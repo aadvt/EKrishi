7c868f49d2e18271d659e3dd8610d76ee0f7df1a
Advaith <advaithinbox@gmail.com>
2026-04-02 12:11:17 +0530

    Add TFLite dependency and register model assets

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -10,6 +10,10 @@ android {
     compileSdk = flutter.compileSdkVersion
     ndkVersion = flutter.ndkVersion
 
+    androidResources {
+        noCompress += "tflite"
+    }
+
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_11
         targetCompatibility = JavaVersion.VERSION_11
@@ -24,7 +28,7 @@ android {
         applicationId = "com.ekrishi.app.ekrishi"
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
-        minSdk = flutter.minSdkVersion
+        minSdk = 26
         targetSdk = flutter.targetSdkVersion
         versionCode = flutter.versionCode
         versionName = flutter.versionName
